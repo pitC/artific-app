7a392f89c3182697ede888d18020c4d9ce01b361
pitC <p.k.cabaj@gmail.com>
2021-10-16 18:11:51 +0100

    Artific frontend migrated to serverless architecture

---
+++ pwa/js/app/sections/colourPicker.js
@@ -0,0 +1,296 @@
+import SharedStorage from "../sharedStorage.js";
+import RouteNames from "../RouteNames.js";
+const MAIN_BT_DEFAULT_LABEL = "Set main colour";
+const SEC_BT_DEFAULT_LABEL = "Set secondary colour";
+const CONTRAST_BT_DEFAULT_LABEL = "Set contrast colour";
+const AVAILABLE_COLOUR_NUM = 3;
+export default {
+  template: `
+<div class="container-fluid">
+<nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
+<button class="navbar-toggler border-0 p-0" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+  aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="ml-auto bd-highlight custon-dropdown-wrapper">
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle custom-switch" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <i class="fas fa-palette"></i>  By colour</button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" v-on:click="onToPhotoPicker"><i class="fas fa-camera"></i> By camera</a>
+  </div>
+</div>
+</div>
+<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="/"><img src="../../../dist/img/logo_bunt.png" alt="artific logo" id="logo-nav" class="img-fluid"></a>
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="/about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/museum.html">For museums</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact.html">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/impressum.html">Legal Disclosure</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/privacypolicy.html">Privacy policy</a>
+          </li>
+          <li class="nav-item">
+          <a
+            class="nav-link"
+            id="som-wrapper"
+            href="https://twitter.com/AppArtific"
+            target="_blank"
+            ><i id="twitter-icon" class="fab fa-twitter pl-lg-3"></i
+          ></a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            id="som-wrapper"
+            href="https://www.instagram.com/artific.app/"
+            target="_blank"
+            ><i id="insta-icon" class="fab fa-instagram pl-lg-3"></i></i
+          ></a>
+        </li>        </ul>
+      </div>
+
+
+</nav>
+  <div class="container">
+    <div class="box box-1">
+      <div class="btn-group mx-4" role="group" aria-label="Basic example">
+        <button
+          type="button"
+          class="btn btn-outline-light btn-lg w-25 mb-3"
+          v-on:click="selectMainColour"
+          :style="mainColourBg"
+        ></button>
+        <button
+          type="button"
+          class="btn btn-outline-light btn-lg w-75 p-3 mb-3"
+          v-on:click="selectMainColour"
+        >
+          {{mainBtLabel}}
+        </button>
+      </div>
+
+      <div class="btn-group mx-4 mb-3" role="group" aria-label="Basic example">
+        <button
+          type="button"
+          class="btn btn-outline-light btn-lg w-25 mb-3"
+          v-on:click="selectSecColour"
+          :style="secColourBg"
+        ></button>
+        <button
+          type="button"
+          class="btn btn-outline-light btn-lg w-75 p-3 mb-3"
+          v-on:click="selectSecColour"
+        >
+          {{secBtLabel}}
+        </button>
+      </div>
+
+      <div class="btn-group mx-4 mb-3" role="group" aria-label="Basic example">
+        <button
+          type="button"
+          class="btn btn-outline-light btn-lg w-25 mb-3"
+          v-on:click="selectContrastColour"
+          :style="contrastColourBg"
+        ></button>
+        <button
+          type="button"
+          class="btn btn-outline-light btn-lg w-75 p-3 mb-3"
+          v-on:click="selectContrastColour"
+        >
+          {{contrastBtLabel}}
+        </button>
+      </div>
+
+      <div class="box box-2">
+        <div class="mt-auto mt-md-0 fixed-bottom">
+          <button type="button" class="btn custom-standard btn-block" :disabled="isCommitDisabled" v-on:click="onCommitColours">
+            Find your art
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade bd-example-modal-sm" id="colourPickModal" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header border-0">
+            <h5 class="modal-title">Choose a colour</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body mx-auto">
+            <div id="colour-pick" class="inl-bl w-100"></div>
+          </div>
+          <div class="modal-footer border-0">
+            
+            <button type="button" class="btn custom-standard btn-block" v-on:click="saveSelection">Save colour</button>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    `,
+  data: function() {
+    return {
+      colours: [null, null, null],
+      selectedColour: 0
+    };
+  },
+  computed: {
+    photoInputRoute() {
+      return RouteNames.PHOTO_INPUT;
+    },
+    mainColourBg() {
+      return "background-color: " + this.colours[0];
+    },
+    secColourBg() {
+      return "background-color: " + this.colours[1];
+    },
+    contrastColourBg() {
+      return "background-color: " + this.colours[2];
+    },
+    mainBtLabel() {
+      if (this.colours[0]) {
+        return this.colours[0].toUpperCase();
+      } else {
+        return MAIN_BT_DEFAULT_LABEL;
+      }
+    },
+    secBtLabel() {
+      if (this.colours[1]) {
+        return this.colours[1].toUpperCase();
+      } else {
+        return SEC_BT_DEFAULT_LABEL;
+      }
+    },
+    contrastBtLabel() {
+      if (this.colours[2]) {
+        return this.colours[2].toUpperCase();
+      } else {
+        return CONTRAST_BT_DEFAULT_LABEL;
+      }
+    },
+    isCommitDisabled() {
+      if (this.colours[0] || this.colours[1] || this.colours[2]) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  },
+  methods: {
+    selectMainColour: function(event) {
+      this.selectColour(0);
+      this.showModal();
+    },
+    selectSecColour: function(event) {
+      this.selectColour(1);
+      this.showModal();
+    },
+    selectContrastColour: function(event) {
+      this.selectColour(2);
+      this.showModal();
+    },
+    saveSelection: function(event) {
+      var colour = $("#colour-pick").colorpicker("getValue", null);
+      this.$set(this.colours, this.selectedColour, colour);
+      this.hideModal();
+    },
+    showModal() {
+      $("#colourPickModal").modal("show");
+    },
+    hideModal() {
+      $("#colourPickModal").modal("hide");
+    },
+    selectColour(id) {
+      this.selectedColour = id;
+
+      var colour = this.colours[id];
+      if (colour) {
+        $("#colour-pick").colorpicker("setValue", colour);
+      }
+    },
+    onToPhotoPicker: function(event){
+      this.$router.push(RouteNames.PHOTO_INPUT);
+    },
+    onCommitColours: function(event) {
+      var selectedColours = this.getManualColours();
+
+      SharedStorage.putInputColours(selectedColours);
+      console.log(selectedColours);
+      this.$router.push({
+        path: RouteNames.RESULT_LIST,
+        query: this.$route.query
+      });
+    },
+    getManualColours: function() {
+      var mainColour = this.colours[0] || "";
+      var secColour = this.colours[1] || "";
+      var contrastColour = this.colours[2] || "";
+      var colours = [];
+      if (mainColour.startsWith("#")) {
+        colours.push(mainColour);
+      }
+      if (secColour.startsWith("#")) {
+        colours.push(secColour);
+      }
+      if (contrastColour.startsWith("#")) {
+        colours.push(contrastColour);
+      }
+      return colours;
+    },
+    photoMode: function(event) {
+      this.$router.push(RouteNames.PHOTO_INPUT);
+    },
+    goBack: function(event) {
+      this.$router.go(-1);
+    }
+  },
+  mounted: function() {
+    var self = this;
+    $(function() {
+      $("#colour-pick").colorpicker({
+        color: "#ffaa00",
+        container: true,
+        inline: true,
+        customClass: "colorpicker-2x",
+        sliders: {
+          saturation: {
+            maxLeft: 200,
+            maxTop: 200
+          },
+          hue: {
+            maxTop: 200
+          },
+          alpha: {
+            maxTop: 200
+          }
+        }
+      });
+    });
+
+    var storedColours = SharedStorage.inputColours;
+    var maxI =
+      storedColours.length > AVAILABLE_COLOUR_NUM
+        ? AVAILABLE_COLOUR_NUM
+        : storedColours.length;
+    for (var i = 0; i < maxI; i++) {
+      this.$set(this.colours, i, storedColours[i]);
+    }
+    console.log("Colour picker mounted!")
+  }
+};
---
+++ pwa/js/app/sections/livePhotoColourPicker.js
@@ -0,0 +1,370 @@
+import { EventBus, EventDict } from "../eventBus.js";
+import InteractiveCanvas from "../canvas/interactiveCanvas.js";
+import SharedStorage from "../sharedStorage.js";
+import RouteNames from "../RouteNames.js";
+const VIDEO_PREVIEW_MODE = "video";
+const COLOUR_PICK_MODE = "colour";
+const PROCESSING_MODE = "processing";
+const MANUAL_UPLOAD_MODE = "manual";
+const WEBCAM_INIT = "init";
+
+var VIDEO_CONSTRAINTS = {
+  audio: false,
+  video: {
+    facingMode: "environment"
+  }
+};
+
+Vue.component("palette-item", {
+  template:
+    "<h3><span class='badge badge-primary' :style='style'>{{colour}} {{name}} ({{population}})</span></h3>",
+  computed: {
+    style() {
+      return "background-color: " + this.colour;
+    }
+  },
+  props: ["colour", "population", "name"]
+});
+
+export default {
+  data: function() {
+    return {
+      interactiveCanvas: null,
+      backgroundImg: null,
+      paintingImg: null,
+      previewHidden: false,
+      draggingMode: false,
+      startMouseX: 0,
+      startMouseY: 0,
+      debugStr: "",
+      scale: 1,
+      markedColour: null,
+      fullPalette: null,
+      state: VIDEO_PREVIEW_MODE,
+      uploadFallback: false,
+      allParsedColours: [],
+      debug: false
+    };
+  },
+  computed: {
+    isVideoHidden() {
+      if (this.state == VIDEO_PREVIEW_MODE) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    isPreviewHidden() {
+      if (this.state == COLOUR_PICK_MODE || this.state == PROCESSING_MODE) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+
+    isProcessing() {
+      if (this.state == PROCESSING_MODE || this.state == WEBCAM_INIT) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    isPreviewNotReady() {
+      if (
+        this.state == PROCESSING_MODE ||
+        this.state == VIDEO_PREVIEW_MODE ||
+        this.state == WEBCAM_INIT
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isUploadHidden() {
+      if (this.state == MANUAL_UPLOAD_MODE) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    isPreviewReady() {
+      return !this.isPreviewNotReady;
+    },
+    style() {
+      return "background-color: " + this.markedColour;
+    },
+    snapshotBtLbl() {
+      if (this.state == WEBCAM_INIT) {
+        return "Starting webcam...";
+      } else if (this.state == PROCESSING_MODE) {
+        return "Crunching pixels...";
+      } else if (this.state == MANUAL_UPLOAD_MODE) {
+        return "Upload picture";
+      } else {
+        return "Take snapshot";
+      }
+    },
+    colourPickerRoute() {
+      return RouteNames.COLOUR_PICKER_INPUT;
+    }
+  },
+  //  //
+  template: `
+  <div class="container-fluid">
+  <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler border-0 p-0" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="ml-auto  bd-highlight custon-dropdown-wrapper">
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle custom-switch" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-camera"></i> By photo</button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" v-on:click="onToManualMode"><i class="fas fa-palette"></i> By colour</a>
+    </div>
+  </div>
+</div>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+  <a class="navbar-brand" href="/"><img src="../../../dist/img/logo_bunt.png" alt="artific logo" id="logo-nav" class="img-fluid"></a>
+  <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <li class="nav-item">
+      <a class="nav-link" href="/about.html">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/museum.html">For museums</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/contact.html">Contact</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/impressum.html">Legal Disclosure</a>
+    </li>
+    <li class="nav-item">
+            <a class="nav-link" href="/privacypolicy.html">Privacy policy</a>
+          </li>
+          <li class="nav-item">
+          <a
+            class="nav-link"
+            id="som-wrapper"
+            href="https://twitter.com/AppArtific"
+            target="_blank"
+            ><i id="twitter-icon" class="fab fa-twitter pl-lg-3"></i
+          ></a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            id="som-wrapper"
+            href="https://www.instagram.com/artific.app/"
+            target="_blank"
+            ><i id="insta-icon" class="fab fa-instagram pl-lg-3"></i></i
+          ></a>
+        </li>
+  </ul>
+</div>
+
+</nav>
+    <p class="bg-light" v-if="debug">{{debugStr}} Dragging:{{draggingMode}}<span class='badge badge-primary' :style='style'>{{markedColour}}</span></p>
+    <div>
+        <video
+          id="video-el"
+          width="100%"
+          autoplay
+          playsinline
+          ref="video"
+          autoplay v-bind:hidden="isVideoHidden"
+        ></video>
+        <canvas width="100%" v-bind:hidden="isPreviewHidden" ref="photoCanvas" @mousedown="onDown" @touchstart="onDown" @mousemove="onMove" @touchmove="onMove" @mouseup="onUp" @touchend="onUp" ></canvas>
+        
+    </div>
+    <div class="container">
+      <div class="box box-1 alert alert-warning" v-bind:hidden="isUploadHidden">
+         <p>
+            Unfortunately we can’t access your camera. But don’t worry, you can
+            still select colours manually.
+          </p>
+          <button type="button" v-on:click="manualMode" class="btn custom-standard">Select colours manually</button>
+      </div>
+      <div class="box box-5 fixed-bottom">
+        <div class="mt-auto w-100 btn-group" role="group">
+          <button id="snapshot-btn" type="button" class="btn custom-standard btn-block" v-on:click="onTakeSnapshot" v-bind:hidden="isPreviewReady" :disabled="isProcessing">
+            <span v-if="isProcessing">
+              <i class="fa fa-spinner fa-spin fa-fw"></i> {{snapshotBtLbl}}
+            </span>
+            <span v-else>
+              <i class="fas fa-camera"></i> {{snapshotBtLbl}}
+            </span>
+          </button>
+    
+          <button id="find-art-btn" type="button" class="btn custom-action btn-block" v-on:click="onCommitColours" v-bind:hidden="isPreviewNotReady" :disabled="isProcessing"><i class="far fa-check-circle"></i> Find your art</button>
+          <button id="videoplay-btn" type="button" class="btn custom-standard btn-block" v-on:click="onRetakeSnapshot" v-bind:hidden="isPreviewNotReady" :disabled="isProcessing"><i class="fas fa-camera"></i> Retake photo</button>
+          </button>
+        </div>
+      </div>
+    </div>
+       <palette-item
+            v-for="swatch in allParsedColours"
+            v-bind:colour="swatch.colour"
+            v-bind:population="swatch.population"
+            v-bind:name="swatch.name"
+    ></palette-item>
+  </div>    
+ 
+    `,
+  // TODO: finish manual upload
+  methods: {
+    onDblClick: function(event) {},
+    onMove: function(event) {
+      if (this.interactiveCanvas) {
+        var pos = this.interactiveCanvas.getPos(event);
+
+        if (this.draggingMode && this.interactiveCanvas.selected) {
+          this.interactiveCanvas.clear();
+          this.interactiveCanvas.drawBackground();
+
+          this.interactiveCanvas.moveSelected(pos.x, pos.y);
+
+          this.interactiveCanvas.drawInteractiveObjects();
+        } else {
+          var colour = this.interactiveCanvas.getPixelColour(pos.x, pos.y);
+          this.markedColour = colour;
+        }
+      }
+      event.preventDefault();
+    },
+    onUp: function(event) {
+      this.draggingMode = false;
+      if (this.interactiveCanvas) {
+        var pos = this.interactiveCanvas.getPos(event);
+        this.interactiveCanvas.releaseShape();
+      }
+    },
+    onDown: function(event) {
+      this.draggingMode = true;
+      if (this.interactiveCanvas) {
+        var pos = this.interactiveCanvas.getPos(event);
+
+        this.interactiveCanvas.selectShape(pos.x, pos.y);
+        this.interactiveCanvas.draw();
+      }
+    },
+    onRetakeSnapshot: function(event) {
+      this.allParsedColours = [];
+      this.interactiveCanvas.unfreeze();
+      if (this.uploadFallback) {
+        this.state = MANUAL_UPLOAD_MODE;
+      } else {
+        this.state = VIDEO_PREVIEW_MODE;
+      }
+    },
+    onToManualMode:function(event){
+      this.$router.push({
+        path: RouteNames.COLOUR_PICKER_INPUT
+      });
+    },
+    onTakeSnapshot: function(event) {
+      var video = this.$refs.video;
+
+      this.debugStr = `client: ${video.clientWidth}/${
+        video.clientHeight
+      } video:${video.videoWidth}/${video.videoHeight}`;
+      this.state = PROCESSING_MODE;
+      var self = this;
+
+      this.interactiveCanvas.takeSnapshot(function(parsedColours) {
+        self.state = COLOUR_PICK_MODE;
+        if (self.debug) {
+          self.allParsedColours = parsedColours.all;
+        }
+      });
+    },
+    onCommitColours: function(event) {
+      var selectedColours = this.interactiveCanvas.getShapeColours();
+
+      SharedStorage.putInputColours(selectedColours);
+      this.$router.push({
+        path: RouteNames.RESULT_LIST,
+        query: this.$route.query
+      });
+    },
+
+    stopVideo: function() {
+      var video = this.$refs.video;
+
+      video.pause();
+      if ("srcObject" in video) {
+        if (video.srcObject) {
+          video.srcObject.getTracks()[0].stop();
+        }
+      } else {
+        video.src.stop();
+      }
+    },
+
+    initCanvas: function(sourceImg, isVideo = false) {
+      if (this.$refs.photoCanvas) {
+        this.interactiveCanvas = new InteractiveCanvas(
+          this.$refs.photoCanvas,
+          sourceImg,
+          isVideo
+        );
+      }
+    },
+
+    manualMode: function(event) {
+      // if upload is shown, we haven't accessed camera so nothing to stop
+      if (this.isUploadHidden) {
+        this.stopVideo();
+      }
+      this.$router.push(RouteNames.COLOUR_PICKER_INPUT);
+    },
+    goBack: function(event) {
+      this.$router.go(-1);
+    }
+  },
+
+  mounted: function() {
+    if (this.$route.query.hasOwnProperty("m")) {
+      if (this.$route.query.m == "debug") {
+        this.debug = true;
+      }
+    }
+
+    var video = this.$refs.video;
+    this.state = WEBCAM_INIT;
+    var self = this;
+
+    // Get access to the camera!
+    // this.mode = VIDEO_PREVIEW_MODE;
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia(VIDEO_CONSTRAINTS)
+        .then(stream => {
+          if ("srcObject" in video) {
+            video.srcObject = stream;
+          } else {
+            video.src = window.URL.createObjectURL(stream);
+          }
+          video.play();
+          this.state = VIDEO_PREVIEW_MODE;
+
+          setTimeout(function() {
+            self.initCanvas(video, true);
+          }, 300);
+        })
+        .catch(function(err) {
+          self.uploadFallback = true;
+          self.state = MANUAL_UPLOAD_MODE;
+        });
+    } else {
+      self.state = MANUAL_UPLOAD_MODE;
+    }
+  },
+
+  beforeRouteLeave: function(to, from, next) {
+    this.stopVideo();
+    next();
+  }
+};
